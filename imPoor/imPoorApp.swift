0b3fe19fb037a4919bc10076870c1ada5f837d7c
alejoortga <cristian-forero@upc.edu.co>
2023-11-27 18:29:13 -0500

    Initial Commit

---
+++ imPoor/imPoorApp.swift
@@ -0,0 +1,17 @@
+//
+//  imPoorApp.swift
+//  imPoor
+//
+//  Created by Cristian Alejandro Ortega on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct imPoorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
